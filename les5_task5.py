3be1647ea837864d80214e90009ebe00f92d59d1
micy-snoozle <26256348+micy-snoozle@users.noreply.github.com>
2021-02-05 08:52:58 -0300

    Add files via upload

---
+++ les5_task5.py
@@ -0,0 +1,19 @@
+# 5. Создать (программно) текстовый файл,
+# записать в него программно набор чисел, разделенных пробелами.
+# Программа должна подсчитывать сумму чисел в файле и выводить ее на экран.
+
+def summary():
+    try:
+        with open('file_2.txt', 'w+', encoding='utf-8') as file_obj:
+            line = input('Введите цифры через пробел: ')
+            file_obj.writelines(line)
+            my_numb = line.split()
+
+            print(sum(map(int, my_numb)))
+    except IOError:
+        print('Ошибка в файле.')
+    except ValueError:
+        print('Неправильно набран номер.')
+
+
+summary()
